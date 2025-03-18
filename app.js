6c24da41df523994d63aa74c6e73b9850a66c89c
Edwin Alejandro Anona <alexedwin1@gmail.com>
2025-03-17 23:10:10 -0600

    Funcion sortear amigo

--- app.js
+++ app.js
@@ -33,3 +33,25 @@ function actualizarListaAmigos() {
     }
     return;
 }
+
+function sortearAmigo() {
+    let longitudArrayAmigos = amigos.length;
+    amigoSorteado = Math.floor(Math.random()*longitudArrayAmigos);
+    
+    console.log(longitudArrayAmigos);
+    console.log(amigoSorteado);
+    if (longitudArrayAmigos != 0) {
+        //no se puede declar una variable aquí, debe ser despues de function.
+        alert(`En tu lista de amigos hay ${longitudArrayAmigos}`)
+        
+        console.log(amigoSorteado);
+        //Aquí en una variable, se asigna el elemento html con el cual se va a interactuar
+        let resultadoSorteo = document.getElementById('resultado');
+        //se le indica a la variable anterior que valor va asignarsele para que aparezca en el elemento html
+        resultadoSorteo.innerHTML = (`El amigo secreto sorteado es: ${amigos[amigoSorteado]}`);
+    } else {
+        console.log('la lista de amigos esta vacía');
+    }
+    return amigoSorteado;
+}
+
